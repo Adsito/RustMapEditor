31b0c92e0cdaf88709ed65b56eb3e67fe3287109
tomtehtom <20974793+tomtehtom@users.noreply.github.com>
2019-04-16 17:07:40 +0100

    Modified UI sizing, anchors, WIP. Added Flexible UI Scripts

---
+++ Assets/Resources/UI/FlexibleDraggableObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(EventTrigger))]
+public class FlexibleDraggableObject : MonoBehaviour
+{
+    public GameObject Target;
+    private EventTrigger _eventTrigger;
+    void Start ()
+    {
+        _eventTrigger = GetComponent<EventTrigger>();
+        _eventTrigger.AddEventTrigger(OnDrag, EventTriggerType.Drag);
+    }
+
+    void OnDrag(BaseEventData data)
+    {
+        PointerEventData ped = (PointerEventData) data;
+        Target.transform.Translate(ped.delta);
+    }
+}
