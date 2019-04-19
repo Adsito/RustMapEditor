fb852f029598d5bdce3306683a152b057c820fca
Adsito <adsito@adsito.net>
2019-04-20 01:46:57 +1000

    Performance Improvements

---
+++ Assets/Scripts/Facepunch/LightLOD.cs
@@ -0,0 +1,19 @@
+// LightLOD
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightLOD : MonoBehaviour
+{
+	private Light lightComponent;
+
+	protected void Awake()
+	{
+		lightComponent = GetComponent<Light>();
+        lightComponent.enabled = false;
+	}
+
+	private void ToggleLight(bool state)
+	{
+        lightComponent.enabled = state;
+    }
+}
