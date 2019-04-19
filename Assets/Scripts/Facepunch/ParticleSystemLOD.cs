3c19bb37c43b7fab9b9cc220f664cafeb431f93e
Adsito <43514196+Adsito@users.noreply.github.com>
2019-04-20 01:46:57 +1000

    Performance Improvements

---
+++ Assets/Scripts/Facepunch/ParticleSystemLOD.cs
@@ -0,0 +1,14 @@
+// ParticleSystemLOD
+using System;
+using UnityEngine;
+
+public class ParticleSystemLOD : MonoBehaviour
+{
+    private ParticleSystem particle;
+
+	protected void Awake()
+    {
+        particle = GetComponent<ParticleSystem>();
+        particle.Stop();
+    }
+}
