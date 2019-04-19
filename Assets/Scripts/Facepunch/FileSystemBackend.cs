eeb5f104683d2914a7d414400ec29f4ba32f92ca
Adsito <43514196+Adsito@users.noreply.github.com>
2019-04-19 23:53:56 +1000

    Revert "Prefab Shaders"
    This reverts commit d88db7c74b488d9320b30e8b3265ca0015e1136c.

--- Assets/Scripts/Facepunch/FileSystemBackend.cs
+++ Assets/Scripts/Facepunch/FileSystemBackend.cs
@@ -65,8 +65,10 @@ public abstract class FileSystemBackend
 		else
 		{
 			val = LoadAsset<T>(filePath);
+            Debug.Log("Loading manifest");
             if (val != null)
 			{
+                Debug.Log("Manifest found");
                 cache.Add(filePath, val);
             }
 		}
