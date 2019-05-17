f7ef76e71e0820b85cca99322bbd929cf3028114
Adsito <43514196+Adsito@users.noreply.github.com>
2019-05-18 04:25:55 +1000

    Node Graph
    - xNode Import and start of Node Graph System.

---
+++ Assets/Editor/Node/TerrainGraph.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+[CreateAssetMenu(fileName = "Terrain Node Editor", menuName = "Terrain Node")]
+public class TerrainGraph : NodeGraph
+{ 
+	
+}
---
+++ Assets/Nodes/Textures/GroundTextureNode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+using NodeVariables;
+
+[CreateNodeMenu("Textures/Ground Texture")]
+public class GroundTextureNode : Node
+{
+    [Output] public GroundTexture Out;
+    public GroundTexture Texture;
+    public override object GetValue(NodePort port)
+    {
+        return Out = Texture;
+    }
+}
