deba82f40f824b05d305ca4735ad8b85dbaeb002
Adsito <adsito@adsito.net>
2019-05-18 04:25:55 +1000

    Node Graph
    - xNode Import and start of Node Graph System.

---
+++ Assets/Nodes/NodeVariables.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace NodeVariables
+{
+    [System.Serializable]
+    public class Anything
+    {
+
+    }
+    public class NodeVariables : MonoBehaviour
+    {
+
+    }
+    public enum GroundTexture
+    {
+        Grass, Dirt, Forest, Sand, Snow, Gravel, Stones, Rock
+    }
+    public enum BiomeTexture
+    {
+        Temperate, Arid, Arctic, Tundra
+    }
+    public enum AlphaTexture
+    {
+        Active, InActive
+    }
+}
