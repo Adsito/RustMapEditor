f221842bc45f822da2a22d19cdd4aa43f0e99f62
Adsito <adsito@adsito.net>
2019-05-20 07:51:53 +1000

    Draggable List + Node + Fixes
    - Added Draggable List package.
    - Added first implementation of the preset list.
    - MapIO UI fixes and more added tooltips.
    - Removed old standalone code from MapIO.

---
+++ Assets/Editor/Node/AutoGenerationGraph.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+[CreateAssetMenu(fileName = "Auto Generation Preset", menuName = "AutoGenPreset")]
+public class AutoGenerationGraph : NodeGraph
+{ 
+	
+}
