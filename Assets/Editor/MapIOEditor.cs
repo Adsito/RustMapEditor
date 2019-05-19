b0d2ade315f5383cec463c017f8981643a4cd896
Adsito <43514196+Adsito@users.noreply.github.com>
2019-05-20 08:45:06 +1000

    v0.9-prerelease

--- Assets/Editor/MapIOEditor.cs
+++ Assets/Editor/MapIOEditor.cs
@@ -8,7 +8,7 @@ using Rotorz.ReorderableList;
 [CustomEditor(typeof(MapIO))]
 public class MapIOEditor : Editor
 {
-    string editorVersion = "v0.7-prerelease";
+    string editorVersion = "v0.9-prerelease";
     string loadFile = "";
     string saveFile = "";
     string mapName = "";
