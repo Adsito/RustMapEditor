c5855c256f3f5e5bb5e7ced9f42e1ddd776f6f78
Adsito <43514196+Adsito@users.noreply.github.com>
2019-05-21 05:46:48 +1000

    Paint Texture Node

---
+++ Assets/Nodes/Editor/PaintLayerNodeEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using XNode;
+using XNodeEditor;
+
+[CustomNodeEditor(typeof(PaintLayerNode))]
+public class PaintLayerNodeEditor : NodeEditor
+{
+    public override void OnBodyGUI()
+    {
+        base.OnBodyGUI();
+        PaintLayerNode node = target as PaintLayerNode;
+        AutoGenerationGraph graph = node.graph as AutoGenerationGraph;
+        NodeVariables.Texture texture = node.GetValue();
+        if (texture != null)
+        {
+            EditorGUILayout.IntField(texture.LandLayer);
+        }
+    }
+}
---
+++ Assets/Nodes/Textures/TextureNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+using NodeVariables;
+
+[CreateNodeMenu("Texture")]
+public class TextureNode : Node
+{
+    [Output] public NodeVariables.Texture Out;
+    public NodeVariables.Texture.LandLayerEnum landLayer;
+    public override object GetValue(NodePort port)
+    {
+        Out.LandLayer = (int)landLayer;
+        return Out.LandLayer;
+    }
+}
