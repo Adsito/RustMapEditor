0265ba3a434610ab40899883978a6c5e04fc8d9a
Adsito <adsito@adsito.net>
2019-06-04 11:09:09 +1000

    v1.3-prerelease

--- Assets/Editor/MapIOEditor.cs
+++ Assets/Editor/MapIOEditor.cs
@@ -9,7 +9,7 @@ using Rotorz.ReorderableList;
 public class MapIOEditor : Editor
 {
 
-    string editorVersion = "v1.2-prerelease";
+    string editorVersion = "v1.3-prerelease";
 
     string loadFile = "";
     string saveFile = "";
