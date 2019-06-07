04b3a2b929060bc196051dc82bea23cf78cb7cc9
Adsito <adsito@adsito.net>
2019-06-07 22:22:30 +1000

    Normalise & Invert HeightMap Nodes
    - Added Normalise & Invert HeightMap Nodes.
    - Fixed Start Node being able to connect to more then 1 node.
    - GUI Fixes.

---
+++ Assets/Scripts/Nodes/Editor/NormaliseHeightMapNodeEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using XNodeEditor;
+
+[CustomNodeEditor(typeof(NormaliseHeightMapNode))]
+public class NormaliseHeightMapNodeEditor : NodeEditor
+{
+    public override Color GetTint()
+    {
+        return Color.cyan;
+    }
+    public override void OnHeaderGUI()
+    {
+        GUILayout.Label(new GUIContent("Normalise HeightMap", "Normalise the heightmap between 2 heights."), NodeEditorResources.styles.nodeHeader, GUILayout.Height(30));
+    }
+    public override void OnBodyGUI()
+    {
+        base.OnBodyGUI();
+        NormaliseHeightMapNode node = target as NormaliseHeightMapNode;
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField(new GUIContent("Low", "The lowest point on the map after being normalised."), GUILayout.MaxWidth(40));
+        EditorGUI.BeginChangeCheck();
+        node.normaliseLow = EditorGUILayout.Slider(node.normaliseLow, 0f, 1000f);
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField(new GUIContent("High", "The highest point on the map after being normalised."), GUILayout.MaxWidth(40));
+        node.normaliseHigh = EditorGUILayout.Slider(node.normaliseHigh, 0f, 1000f);
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField(new GUIContent("Blend", "The amount of blending to occur during normalisation. The higher the value the" +
+            "smoother the result will be."), GUILayout.MaxWidth(40));
+        node.normaliseBlend = EditorGUILayout.Slider(node.normaliseBlend, 0f, 1f);
+        EditorGUILayout.EndHorizontal();
+    }
+}
--- Assets/Scripts/Nodes/Editor/RotateHeightMapNodeEditor.cs
+++ Assets/Scripts/Nodes/Editor/RotateHeightMapNodeEditor.cs
@@ -11,12 +11,12 @@ public class RotateHeightMapNodeEditor : NodeEditor
     }
     public override void OnHeaderGUI()
     {
-        GUILayout.Label(new GUIContent("Rotate Layer", "Rotates the layer of the inputted texture."), NodeEditorResources.styles.nodeHeader, GUILayout.Height(30));
+        GUILayout.Label(new GUIContent("Rotate HeightMap", "Rotates the heightmap and watermap."), NodeEditorResources.styles.nodeHeader, GUILayout.Height(30));
     }
     public override void OnBodyGUI()
     {
         base.OnBodyGUI();
         RotateHeightMapNode node = target as RotateHeightMapNode;
-        node.CW = EditorGUILayout.ToggleLeft(new GUIContent(node.CW ? "Rotate Direction: 90°" : "Rotate Direction: 270°", "The direction which the heightmap will rotate, either 90° or 270°"), node.CW);
+        node.CW = EditorGUILayout.ToggleLeft(new GUIContent(node.CW ? "Rotate Direction: 90°" : "Rotate Direction: 270°", "The direction which the heightmap and watermap will rotate, either 90° or 270°"), node.CW);
     }
 }
