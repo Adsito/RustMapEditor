9f64a0487aa42243a9f29fb9a6adc1ad20512e0f
Adsito <43514196+Adsito@users.noreply.github.com>
2019-06-09 23:54:03 +1000

    SetMinimum + SetMaximum Height Nodes
    - Added SetMinimum and SetMaximum nodes to the node editor.

---
+++ Assets/Scripts/Nodes/Editor/SetMinimumHeightNodeEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using XNodeEditor;
+
+[CustomNodeEditor(typeof(SetMinimumHeight))]
+public class SetMinimumHeightEditor : NodeEditor
+{
+    public override Color GetTint()
+    {
+        return Color.cyan;
+    }
+    public override void OnHeaderGUI()
+    {
+        GUILayout.Label(new GUIContent("Set Minimum Height", "Sets any part of the heightmap below to the minimum height."), NodeEditorResources.styles.nodeHeader, GUILayout.Height(30));
+    }
+    public override void OnBodyGUI()
+    {
+        base.OnBodyGUI();
+        SetMinimumHeight node = target as SetMinimumHeight;
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField(new GUIContent("Height", "The minimum height the heightmap should be. Will raise any part of the heightmap below this " +
+            "height to the minimum height set."), GUILayout.MaxWidth(50));
+        node.minimumHeight = EditorGUILayout.Slider(node.minimumHeight, 0f, 1000f);
+        EditorGUILayout.EndHorizontal();
+    }
+}
