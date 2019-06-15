b8f76591f1862c82d5065a79f65d9522bee8425a
Adsito <adsito@adsito.net>
2019-06-16 02:24:41 +1000

    Node System Serialization
    - All nodes and their variables are now always serialized.
    - Fixed some cases where loading a saved preset would set the values back to default.
    - Moved all the variables out of the editor scripts.

--- Assets/Scripts/Nodes/Textures/TextureNode.cs
+++ Assets/Scripts/Nodes/Textures/TextureNode.cs
@@ -1,10 +1,18 @@
-﻿using XNode;
+﻿using UnityEngine;
+using XNode;
 
 [CreateNodeMenu("Texture")]
 public class TextureNode : Node
 {
     [Output] public NodeVariables.Texture Texture;
+    #region Fields
     public NodeVariables.Texture.LandLayerEnum landLayer;
+    [HideInInspector] public TerrainSplat.Enum groundEnum = TerrainSplat.Enum.Grass;
+    [HideInInspector] public TerrainBiome.Enum biomeEnum = TerrainBiome.Enum.Temperate;
+    [HideInInspector] public NodeVariables.Texture.AlphaEnum alphaEnum;
+    [HideInInspector] public TerrainTopology.Enum topologyLayer = TerrainTopology.Enum.Beach;
+    [HideInInspector] public NodeVariables.Texture.TopologyEnum topologyEnum;
+    #endregion
     public override object GetValue(NodePort port)
     {
         return Texture;
