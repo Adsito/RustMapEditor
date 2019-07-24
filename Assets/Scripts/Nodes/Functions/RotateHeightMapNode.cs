4e95ae29f56114254178b7cd792b6e4a4e4f60ec
Adsito <43514196+Adsito@users.noreply.github.com>
2019-07-25 01:33:18 +1000

    Node Fixes + MapIOUI fixes
    - Rotate HeightMapNode now uses the rotate enum.
    - Removed the spawn prefabs tab from the UI, and added a warning for loading prefabs.

--- Assets/Scripts/Nodes/Functions/RotateHeightMapNode.cs
+++ Assets/Scripts/Nodes/Functions/RotateHeightMapNode.cs
@@ -6,13 +6,14 @@ public class RotateHeightMapNode : Node
 {
     [Input(ShowBackingValue.Never, ConnectionType.Override, TypeConstraint.Strict)] public NodeVariables.NextTask PreviousTask;
     [Output(ShowBackingValue.Never, ConnectionType.Override, TypeConstraint.Strict)] public NodeVariables.NextTask NextTask;
-    [HideInInspector] public bool CW = true;
+    public NodeVariables.Misc.RotateDirection direction = NodeVariables.Misc.RotateDirection.ClockWise;
     public override object GetValue(NodePort port)
     {
         return null;
     }
     public void RunNode()
     {
+        bool CW = (direction == NodeVariables.Misc.RotateDirection.ClockWise) ? true : false;
         MapIO.RotateHeightmap(CW);
     }
 }
