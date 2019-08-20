d2a0d6e396c6ed419df55f02e47da34bf9144d59
Adsito <43514196+Adsito@users.noreply.github.com>
2019-08-21 01:36:12 +1000

    SDK Update
    - Updated files to match WorldSerialization2 branch.
    - Removed old unused placeholder facepunch scripts.

--- Assets/Scripts/Facepunch/Socket_Terrain.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-[ExecuteAlways]
-public class Socket_Terrain : MonoBehaviour
-{
-    
-}
--- Assets/Scripts/Facepunch/TerrainCollisionTrigger.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-[ExecuteAlways]
-public class TerrainCollisionTrigger : MonoBehaviour
-{
-    
-}
