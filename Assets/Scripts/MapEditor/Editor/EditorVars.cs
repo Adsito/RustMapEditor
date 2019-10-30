5a98b3425ff68feaed9bc4dcdbfd46b874c334ca
Adsito <adsito@adsito.net>
2019-10-30 18:24:20 +1100

    EditorUI progress
    - Added ToolbarMinMaxInt and ToolbarIntSliders to EditorUI.
    - Moved Area Tools over to new ToolbarMinMaxInt UI.

--- Assets/Scripts/MapEditor/Editor/EditorVars.cs
+++ Assets/Scripts/MapEditor/Editor/EditorVars.cs
@@ -10,6 +10,11 @@ public static class EditorVars
         public static GUIContent rangeLow = new GUIContent("From:", "The lowest value to paint the active texture.");
         public static GUIContent rangeHigh = new GUIContent("To:", "The highest value to paint the active texture.");
         public static GUIContent blendLow = new GUIContent("Blend Low:", "The lowest value to blend out to.");
-        public static GUIContent blendHigh= new GUIContent("Blend High:", "The highest value to blend out to.");
+        public static GUIContent blendHigh = new GUIContent("Blend High:", "The highest value to blend out to.");
+
+        public static GUIContent fromZ = new GUIContent("From Z", "The starting point to paint the active texture.");
+        public static GUIContent toZ = new GUIContent("To Z", "The ending point to paint the active texture.");
+        public static GUIContent fromX = new GUIContent("From X", "The starting point to paint the active texture.");
+        public static GUIContent toX = new GUIContent("To X", "The ending point to paint the active texture.");
     }
 }
