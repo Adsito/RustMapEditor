1e403b4ba6970d99e9fdfacab633211605d84a40
Adsito <43514196+Adsito@users.noreply.github.com>
2019-11-01 20:28:30 +1100

    Move PrefabData to top of list test

--- Assets/Scripts/MapEditor/Editor/EditorUIFunctions.cs
+++ Assets/Scripts/MapEditor/Editor/EditorUIFunctions.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EditorUIFunctions : MonoBehaviour
+public static class EditorUIFunctions 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
