6d43d75b011c18c091402e5c7477f2ba44525fbf
Adsito <43514196+Adsito@users.noreply.github.com>
2019-11-06 23:43:33 +1100

    RendererLOD SetLODs
    - Added SetLODs to RendererLOD, called on first spawn of prefab.
    This is temporary and might be replaced by updating the meshes based on camera distance like in Rust.
    - Added base MeshLOD class (doesn't do anything yet)
    - PrefabManager now checks for if a bundle is loaded before trying to load a bundle.
    - PrefabDataID input field is now text, and converts back to uint after the user presses enter.

--- Assets/Scripts/Facepunch/RustWorldSDK/RendererLOD.cs
+++ Assets/Scripts/Facepunch/RustWorldSDK/RendererLOD.cs
@@ -4,7 +4,8 @@ using UnityEngine.Rendering;
 
 public class RendererLOD : MonoBehaviour
 {
-    public class State
+	[Serializable]
+	public class State
 	{
 		public float distance;
 
@@ -19,9 +20,30 @@ public class RendererLOD : MonoBehaviour
 		[NonSerialized]
 		public bool isImpostor;
 	}
-    public State[] States;
-    public void Start()
+	public State[] States;
+
+	private int curlod;
+
+	private bool force;
+
+    public void SetLODS()
     {
-        Debug.Log(GetComponent<RendererLOD.State>().distance);
+        if (gameObject.GetComponent<LODGroup>() == null)
+        {
+            var lodGroup = gameObject.AddComponent<LODGroup>();
+            var statesLength = States.Length - 1;
+            LOD[] lods = new LOD[statesLength];
+            for (int i = 0; i < statesLength; i++)
+            {
+                if (States[i].renderer != null)
+                {
+                    States[i].renderer.enabled = true;
+                }
+                lods[i] = new LOD(1.0F / (i + 5f), new Renderer[] { States[i].renderer });
+            }
+            lodGroup.SetLODs(lods);
+            lodGroup.fadeMode = LODFadeMode.SpeedTree;
+            lodGroup.RecalculateBounds();
+        }
     }
 }
