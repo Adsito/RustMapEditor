5b1b6bd859d66a443adabb61a8afa2481a9f16dc
Adsito <43514196+Adsito@users.noreply.github.com>
2019-11-12 23:04:56 +1100

    Prefab Hierachy Initial
    - Added EditorTreeview namespace and backend API.
    - Added basic PrefabHierachyWindow which displays info about all the prefabs currently loaded on the map.
    - Updated Unity to 2019.2.12f1.
    - MapIO now creates new map on each project launch and after assembly recompile.
    - Removed (Clone) tag on prefabs loaded onto the map.

---
+++ Assets/Scripts/MapEditor/Editor/TreeView/PrefabHierachy/PrefabHierachyElement.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+namespace EditorTreeView
+{
+	[Serializable]
+	internal class PrefabHierachyElement : TreeElement
+	{
+	    public string prefabName, type, category;
+        public uint rustID;
+
+		public PrefabHierachyElement (string name, int depth, int id) : base (name, depth, id)
+		{
+            if (!String.IsNullOrEmpty(name))
+            {
+                var values = name.Split(':');
+                prefabName = values[0];
+                type = values[1];
+                category = values[2];
+                rustID = uint.Parse(values[3]);
+            }
+        }
+	}
+}
