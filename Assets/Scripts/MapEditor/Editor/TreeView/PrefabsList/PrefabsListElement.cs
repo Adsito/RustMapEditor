bfee72409fd525da6bf53e236dd590410643b9df
Adsito <43514196+Adsito@users.noreply.github.com>
2020-01-11 15:36:27 +1100

    PrefabList Init
    - Added PrefabsListWindow TreeView.
    Currently loads all manifest strings into a treeview and parents based on directory path.

---
+++ Assets/Scripts/MapEditor/Editor/TreeView/PrefabsList/PrefabsListElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RustMapEditor.UI
+{
+    [Serializable]
+    internal class PrefabsListElement : TreeElement
+    {
+        public string prefabName;
+        public uint RustID;
+
+        public PrefabsListElement(string name, int depth, int id) : base(name, depth, id)
+        {
+            if (!String.IsNullOrEmpty(name))
+            {
+                prefabName = name;
+            }
+        }
+    }
+}
