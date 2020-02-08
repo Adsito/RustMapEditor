ba8c66cefbf521d7be920f97d4bad88530782df1
Adsito <43514196+Adsito@users.noreply.github.com>
2020-02-08 11:57:12 +1100

    ShortcutManager
    - Added initial implementation of a shortcut manager.

---
+++ Assets/Scripts/MapEditor/Editor/Managers/ShortcutManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.ShortcutManagement;
+using RustMapEditor.UI;
+
+public static class ShortcutManager
+{
+    [Shortcut("RustMapEditor/Load Map")]
+    public static void LoadMap()
+    {
+        Functions.LoadMapPanel();
+    }
+
+    [Shortcut("RustMapEditor/Save Map")]
+    public static void SaveMap()
+    {
+        Functions.SaveMapPanel();
+    }
+
+    [Shortcut("RustMapEditor/New Map")]
+    public static void NewMap()
+    {
+        Functions.NewMapPanel();
+    }
+
+}
