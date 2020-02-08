fbb890fbe7263d00715c9d19065fc6556952cc76
Adsito <43514196+Adsito@users.noreply.github.com>
2020-02-08 19:29:10 +1100

    CreateMapWindow options
    - Moved CreateMapWindow GUI logic into UI.Functions class.
    - Added some options to control when creating new maps (land height, ground and biome textures).

--- Assets/Scripts/MapEditor/Editor/Windows/CreateMapWindow.cs
+++ Assets/Scripts/MapEditor/Editor/Windows/CreateMapWindow.cs
@@ -6,22 +6,13 @@ using RustMapEditor.Variables;
 public class CreateMapWindow : EditorWindow
 {
     int mapSize = 1000;
+    Layers layers = new Layers() { Ground = TerrainSplat.Enum.Grass, Biome = TerrainBiome.Enum.Temperate };
+    float landHeight = 505;
 
     public static void Init()
     {
-        int newMap = EditorUtility.DisplayDialogComplex("Warning", "Creating a new map will remove any unsaved changes to your map.", "Create New Map", "Close", "Save and Create New Map");
-
-        switch (newMap)
-        {
-            case 1:
-                return;
-            case 2:
-                Functions.SaveMapPanel();
-                break;
-        }
-
         CreateMapWindow window = CreateInstance<CreateMapWindow>();
-        window.position = new Rect(ScenePos().x + ScenePos().width / 2 - 75f, ScenePos().y + ScenePos().height / 2, 200f, 60f);
+        window.position = new Rect(ScenePos().x + ScenePos().width / 2 - 75f, ScenePos().y + ScenePos().height / 2, 250f, 123f);
         window.ShowPopup();
     }
 
@@ -29,17 +20,7 @@ public class CreateMapWindow : EditorWindow
     {
         Elements.BoldLabel(ToolTips.createMapLabel);
 
-        mapSize = Elements.ToolbarIntSlider(ToolTips.mapSize, mapSize, 1000, 6000);
-
-        Elements.BeginToolbarHorizontal();
-        if (Elements.ToolbarButton(ToolTips.createMap))
-        {
-            MapIO.CreateMap(mapSize);
-            Close();
-        }
-        if (Elements.ToolbarButton(ToolTips.cancel))
-            Close();
-        Elements.EndToolbarHorizontal();
+        Functions.NewMapOptions(ref mapSize, ref landHeight, ref layers, this);
     }
 
     static Rect ScenePos()
