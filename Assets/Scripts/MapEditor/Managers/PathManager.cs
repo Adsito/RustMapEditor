ecc0aef19558e343284d015e80f1a8ff7646f8e7
Adsito <adsito@adsito.net>
2020-02-09 22:47:35 +1100

    PathManager
    - Added class to handle spawning of path objects in the map. (Currently only handling the spawning of paths on map load.)
    - Moved Managers folder out of scripts folder.

---
+++ Assets/Scripts/MapEditor/Managers/PathManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using static WorldSerialization;
+
+public static class PathManager
+{
+    public static GameObject defaultPath { get; private set; }
+    public static GameObject defaultNode { get; private set; }
+    public static Transform pathParent { get; private set; }
+
+    [InitializeOnLoadMethod]
+    public static void Init()
+    {
+        EditorApplication.update += OnProjectLoad;
+    }
+
+    static void OnProjectLoad()
+    {
+        defaultPath = Resources.Load<GameObject>("Paths/Path");
+        defaultNode = Resources.Load<GameObject>("Paths/PathNode");
+        pathParent = GameObject.FindGameObjectWithTag("Paths").transform;
+        EditorApplication.update -= OnProjectLoad;
+    }
+
+    public static void Spawn(PathData pathData)
+    {
+        Vector3 averageLocation = Vector3.zero;
+        for (int j = 0; j < pathData.nodes.Length; j++)
+        {
+            averageLocation += pathData.nodes[j];
+        }
+        averageLocation /= pathData.nodes.Length;
+        GameObject newObject = GameObject.Instantiate(defaultPath, averageLocation + pathParent.position, Quaternion.identity, pathParent);
+
+        List<GameObject> pathNodes = new List<GameObject>();
+        for (int j = 0; j < pathData.nodes.Length; j++)
+        {
+            GameObject newNode = GameObject.Instantiate(defaultNode, newObject.transform);
+            newNode.transform.position = pathData.nodes[j] + pathParent.position;
+            pathNodes.Add(newNode);
+        }
+        newObject.GetComponent<PathDataHolder>().pathData = pathData;
+    }
+}
