6dd7db831128e714ff7a882e158b0d858baac0da
Adsito <43514196+Adsito@users.noreply.github.com>
2020-02-15 02:42:16 +1100

    SceneObjectHideFlags
    - Added basic class to allow changing the HideFlags on objects in the scene and hierachy.

---
+++ Assets/Scripts/MapEditor/Utilities/SceneObjectHideFlags.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class SceneObjectHideFlags : MonoBehaviour
+{
+    public HideFlags Flags;
+
+    void Start()
+    {
+        gameObject.hideFlags = Flags;
+    }
+}
