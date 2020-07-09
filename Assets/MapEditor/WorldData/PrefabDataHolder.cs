8008bb139ac687981c164fcadb69dba05bfe4229
Adsito <43514196+Adsito@users.noreply.github.com>
2020-07-09 10:34:51 +1000

    PrefabData Setup
    - Removed Start method from PrefabDataHolder, replaced with setup method which is only called once per unique prefab, rather than every new instance of PrefabDataHolder. (Increases prefab spawning by ~20%)

--- Assets/MapEditor/WorldData/PrefabDataHolder.cs
+++ Assets/MapEditor/WorldData/PrefabDataHolder.cs
@@ -5,24 +5,23 @@ using static RustMapEditor.Data.TerrainManager;
 
 [DisallowMultipleComponent]
 [SelectionBase]
-[ExecuteAlways]
 public class PrefabDataHolder : MonoBehaviour
 {
     public WorldSerialization.PrefabData prefabData;
 
-    private void Start()
+    public void Setup()
     {
         for (int i = 0; i < GetComponents<Component>().Length; i++)
-        {
             ComponentUtility.MoveComponentUp(this);
-        }
     }
+
     public void UpdatePrefabData()
     {
         prefabData.position = gameObject.transform.position - (0.5f * land.terrainData.size);
         prefabData.rotation = transform.rotation;
         prefabData.scale = transform.localScale;
     }
+
     public void SnapToGround()
     {
         Vector3 newPos = transform.position;
