e12e124c4d6f694e12079b62b5c9a149d6b2713d
Adsito <adsito@adsito.net>
2020-07-17 22:38:30 +1000

    SceneManager
    - Added SceneManager class, currently monitors the hierachy and parents items that are cloned outside of the parented objects.

---
+++ Assets/MapEditor/Managers/SceneManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public static class SceneManager
+{
+    public static Scene EditorScene { get; private set; }
+
+    [InitializeOnLoadMethod]
+    private static void Init()
+    {
+        EditorApplication.update += OnProjectLoad;
+        EditorApplication.hierarchyChanged += OnSceneChanged;
+    }
+
+    private static void OnProjectLoad()
+    {
+        EditorScene = EditorSceneManager.GetActiveScene();
+        if (EditorScene.IsValid())
+            EditorApplication.update -= OnProjectLoad;
+    }
+
+    private static void OnSceneChanged()
+    {
+        if (EditorScene.rootCount != 4)
+        {
+            foreach (var item in EditorScene.GetRootGameObjects())
+            {
+                if (item.TryGetComponent(out PrefabDataHolder prefab))
+                {
+                    prefab.gameObject.transform.SetParent(PrefabManager.PrefabParent);
+                    continue;
+                }
+                if (item.TryGetComponent(out PathDataHolder path))
+                    path.gameObject.transform.SetParent(PathManager.PathParent);
+            }
+        }
+    }
+}
