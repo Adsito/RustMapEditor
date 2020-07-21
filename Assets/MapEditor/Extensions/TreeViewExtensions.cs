938432fb942b1ec4ce6ad33f69157ab39ab6a6ca
Adsito <adsito@adsito.net>
2020-07-22 01:10:54 +1000

    PrefabHierachy Options
    - Added side panel to PrefabHierachyTreeView. Currently adds delete prefabs option.
    - Cleaned up some misc treeview code.

--- Assets/MapEditor/Extensions/TreeViewExtensions.cs
+++ Assets/MapEditor/Extensions/TreeViewExtensions.cs
@@ -7,24 +7,16 @@ public static class TreeViewExtensions
 	public static IOrderedEnumerable<T> Order<T, TKey>(this IEnumerable<T> source, Func<T, TKey> selector, bool ascending)
 	{
 		if (ascending)
-		{
 			return source.OrderBy(selector);
-		}
 		else
-		{
 			return source.OrderByDescending(selector);
-		}
 	}
 
 	public static IOrderedEnumerable<T> ThenBy<T, TKey>(this IOrderedEnumerable<T> source, Func<T, TKey> selector, bool ascending)
 	{
 		if (ascending)
-		{
 			return source.ThenBy(selector);
-		}
 		else
-		{
 			return source.ThenByDescending(selector);
-		}
 	}
 }
