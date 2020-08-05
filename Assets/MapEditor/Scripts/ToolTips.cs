c0db18597130d8337dbeff6ba1bcbf11689a13c0
Adsito <43514196+Adsito@users.noreply.github.com>
2020-08-05 16:30:15 +1000

    Revert "Update ToolTips.cs"
    This reverts commit 0438067599ae052662b6b2e46372d6dfafbe625d.

--- Assets/MapEditor/Scripts/ToolTips.cs
+++ Assets/MapEditor/Scripts/ToolTips.cs
@@ -92,7 +92,7 @@ namespace RustMapEditor.Variables
 
         public static GUIContent snapToGround = new GUIContent("Snap To Ground", "Snap the selected prefab to the terrain height.");
         public static GUIContent toggleLights = new GUIContent("Toggle Lights", "Toggles all the lights in the selected prefab on/off.");
-        public static GUIContent breakPrefab = new GUIContent("Debug Info", "Test logs prefab data.");
+        public static GUIContent breakPrefab = new GUIContent("Break Prefab", "Breaks down a prefab into the individual prefabs, where possible.");
 
         public static GUIContent toggleBlend = new GUIContent("Blend", "Blends out the active texture to create a smooth transition the surrounding textures.");
         public static GUIContent rangeLow = new GUIContent("From:", "The lowest value to paint the active texture.");
