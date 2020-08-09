b136f5cff16be98a421b3236cc9be8c230ea1947
Adsito <43514196+Adsito@users.noreply.github.com>
2020-08-09 17:54:38 +1000

    EventManager Init
    - Added class to store and handle Editor wide callbacks.

---
+++ Assets/MapEditor/Managers/EventManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class EventManager
+{
+    public delegate void AssetManagerCallback();
+
+    /// <summary>Called after Rust Asset Bundles are loaded into the editor. </summary>
+    public static event AssetManagerCallback BundlesLoaded;
+
+    /// <summary>Called after Rust Asset Bundles are unloaded from the editor. </summary>
+    public static event AssetManagerCallback BundlesDisposed;
+
+    public static void OnBundlesLoaded() => BundlesLoaded.Invoke();
+
+    public static void OnBundlesDisposed() => BundlesDisposed.Invoke();
+}
