c1578d5040ecc1c7e8c213a50d1b2fe9ea4adbfa
Adsito <43514196+Adsito@users.noreply.github.com>
2020-08-25 01:06:36 +1000

    ProgressManager
    - Stores method to remove old task progress.

---
+++ Assets/MapEditor/Managers/ProgressManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+public static class ProgressManager
+{
+    /// <summary>Removes any finished progress bars with the same name.</summary>
+    public static void RemoveProgressBars(string progressName)
+    {
+        for (int i = 0; i < Progress.GetCount(); i++)
+        {
+            var progress = Progress.GetProgressById(Progress.GetId(i));
+            if (progress.finished && progress.name.Contains(progressName))
+                progress.Remove();
+        }
+    }
+}
