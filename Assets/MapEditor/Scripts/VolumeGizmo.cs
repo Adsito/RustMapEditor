9ab0faf48cbbf09a07d05c804c9c527dafcd90a9
Adsito <43514196+Adsito@users.noreply.github.com>
2020-08-31 19:10:15 +1000

    VolumeGizmos Init

---
+++ Assets/MapEditor/Scripts/VolumeGizmo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[ExecuteAlways]
+public class VolumeGizmo : MonoBehaviour
+{
+    //[HideInInspector]
+    public Mesh mesh;
+
+    private void OnDrawGizmos()
+    {
+        if (Vector3.Distance(gameObject.transform.position, SceneView.lastActiveSceneView.camera.transform.position) <= SettingsManager.PrefabRenderDistance)
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireMesh(mesh, 0, gameObject.transform.position, gameObject.transform.rotation, gameObject.transform.lossyScale);
+        }
+    }
+}
