4d004e92c4e5823236e2da41f701ec86d03816c2
Adsito <43514196+Adsito@users.noreply.github.com>
2020-09-26 17:26:58 +1000

    CustomPrefab Init

---
+++ Assets/MapEditor/WorldData/CustomPrefabHolder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase, DisallowMultipleComponent]
+public class CustomPrefabHolder : MonoBehaviour
+{
+    public CustomPrefab CustomPrefab { get; private set; }
+
+    public void Setup(string name, PrefabDataHolder[] prefabs)
+    {
+        var prefabsList = new List<CustomPrefab.PrefabData>();
+        foreach (var item in prefabs)
+            prefabsList.Add(new CustomPrefab.PrefabData { Prefab = item.prefabData });
+
+        CustomPrefab = new CustomPrefab { Data = new CustomPrefab.Base { Name = name, Prefabs = prefabsList } };
+    }
+
+    public void Update()
+    {
+        if (gameObject.transform.childCount == 0)
+            GameObject.DestroyImmediate(gameObject);
+    }
+}
